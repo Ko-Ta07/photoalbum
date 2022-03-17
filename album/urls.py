05628567bb34d24857821cc857ef9089be092701
KOTA <sakamichi.hinatazaka.46@gmail.com>
2022-03-17 15:16:16 +0900

    first-commit

---
+++ album/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .views import (PhotoListView, PhotoDetailView, TagPhotoListView, PhotoCreateView)
+
+urlpatterns = [
+    path("", PhotoListView.as_view(), name="photo-list"),
+    path("tag/<str:tag>/",TagPhotoListView.as_view(), name="tag-photo-list"),
+    path("photo/<int:pk>",PhotoDetailView.as_view(), name="photo-detail"),
+    path("create/",PhotoCreateView.as_view(), name="photo-create"),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
